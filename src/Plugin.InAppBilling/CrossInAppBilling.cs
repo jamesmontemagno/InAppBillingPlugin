9aa3fe13deeb80d6335d79f67c4616a24de83b30
James Montemagno <james.montemagno@gmail.com>
2017-06-20 13:53:39 -0700

    Upgrade to net standard 1.0 properly

--- src/Plugin.InAppBilling/CrossInAppBilling.cs
+++ src/Plugin.InAppBilling/CrossInAppBilling.cs
@@ -28,7 +28,7 @@ namespace Plugin.InAppBilling
 
         static IInAppBilling CreateInAppBilling()
         {
-#if PORTABLE
+#if NETSTANDARD1_0
             return null;
 #else
             return new InAppBillingImplementation();
