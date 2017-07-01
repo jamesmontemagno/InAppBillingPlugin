e4af71d1098cf2d044f4179fe8e59a3dfe18f964
James Montemagno <james.montemagno@gmail.com>
2017-07-01 16:23:46 -0700

    update assembly info

--- src/Plugin.InAppBilling.tvOS/Properties/AssemblyInfo.cs
+++ src/Plugin.InAppBilling.tvOS/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Plugin.InAppBilling.tvOS")]
+[assembly: AssemblyTitle("Plugin.InAppBilling")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Plugin.InAppBilling.tvOS")]
+[assembly: AssemblyProduct("Plugin.InAppBilling")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
