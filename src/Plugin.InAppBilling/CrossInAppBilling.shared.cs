1c0509a6aadbf4b9f5257bc512e598b268acd806
James Montemagno <james.montemagno@gmail.com>
2022-09-01 12:28:04 -0700

    push netstandard

--- src/Plugin.InAppBilling/CrossInAppBilling.shared.cs
+++ src/Plugin.InAppBilling/CrossInAppBilling.shared.cs
@@ -33,7 +33,7 @@ namespace Plugin.InAppBilling
 
         static IInAppBilling CreateInAppBilling()
         {
-#if NETSTANDARD1_0 || NETSTANDARD2_0
+#if NETSTANDARD1_0 || NETSTANDARD2_0 || NETSTANDARD
             return null;
 #else
 #pragma warning disable IDE0022 // Use expression body for methods
